308a03421c161dca2b8bf2558a51131030d17597
Conghao Fu <fch23@mails.tsinghua.edu.cn>
2025-07-08 01:35:31 +0800

    Delete Whisper.py

--- Whisper.py
+++
@@ -1,8 +0,0 @@
-import whisper
-
-model_path = 'model\\whisper\\large-v3.pt'
-model = whisper.load_model(model_path, device="cuda")
-
-result = model.transcribe("example/1.mp3")
-
-print(result["text"])
