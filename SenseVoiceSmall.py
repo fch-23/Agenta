7d1ed70b83162444916fc4c25ab5ccb84043a033
Conghao Fu <fch23@mails.tsinghua.edu.cn>
2025-07-09 16:54:19 +0800

    Add files via upload

--- SenseVoiceSmall.py
+++ SenseVoiceSmall.py
@@ -8,7 +8,7 @@ model_dir = "model/SenseVoiceSmall"
 model = AutoModel(
     model=model_dir,
     trust_remote_code=False,  # 禁用远程代码加载
-    vad_model="fsmn-vad",
+    vad_model="model/speech_fsmn_vad_zh-cn-16k-common-pytorch",
     vad_kwargs={"max_single_segment_time": 30000},
     device="cuda:0",
     disable_tqdm=True,  # 禁用进度条
@@ -17,7 +17,7 @@ model = AutoModel(
 
 # 处理音频文件
 res = model.generate(
-    input=f"example/1.mp3",
+    input=f"example/json.mp3",
     cache={},
     language="auto",  # 自动检测语言
     use_itn=True,
@@ -28,4 +28,7 @@ res = model.generate(
 
 # 后处理并输出结果
 text = rich_transcription_postprocess(res[0]["text"])
-print(text)
+
+# 将结果保存到 result 文件中
+with open("result1.txt", "w", encoding="utf-8") as f:
+    f.write(text)
